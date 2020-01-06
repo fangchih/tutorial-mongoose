e38b02c091881cfd8b8cde8248f220a31e7c6a33
fangchih <fangchih.huang@gmail.com>
2020-01-06 15:22:24 +0800

    add testing data

--- models/author.js
+++ models/author.js
@@ -5,8 +5,7 @@ var Schema = mongoose.Schema;
 var AuthorSchema = new Schema(
   {
     first_name: {type: String, required: true, max: 100},
-    family_name: {type: String, required: true, max: 100},
-    date_of_birth: {type: Date},
+    family_name: {type: String, required: true, max: 100},    date_of_birth: {type: Date},
     date_of_death: {type: Date},
   }
 );
@@ -15,7 +14,26 @@ var AuthorSchema = new Schema(
 AuthorSchema
 .virtual('name')
 .get(function () {
-  return this.family_name + ', ' + this.first_name;
+
+// To avoid errors in cases where an author does not have either a family name or first name
+// We want to make sure we handle the exception by returning an empty string for that case
+
+  var fullname = '';
+  if (this.first_name && this.family_name) {
+    fullname = this.family_name + ', ' + this.first_name
+  }
+  if (!this.first_name || !this.family_name) {
+    fullname = '';
+  }
+
+  return fullname;
+});
+
+// Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
 });
 
 // Virtual for author's URL
--- testmongoose.js
+++
@@ -1,7 +0,0 @@
-//Set up mongoose connection
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://hbs:hbs1234@cluster0-63tdc.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
-mongoose.connect(mongoDB);
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
